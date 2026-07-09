005a7ae352a7641124a090ba604a947feb9f6002
Naty Macias <st4441@utr.edu.mx>
2026-07-09 12:04:45 -0600

    Configuración para el despliegue en vercel

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const connectDB = require("./src/config/database");
 
 const bookRoutes = require("./src/routes/bookRoutes");
-const cartRoutes = require("./src/routes/cartRoutes");
 const userRoutes = require("./src/routes/userRoutes");
+const cartRoutes = require("./src/routes/cartRoutes");
 const orderRoutes = require("./src/routes/orderRoutes");
 
 const app = express();
@@ -19,11 +19,16 @@ app.use(express.json());
 
 // Rutas
 app.use("/api/books", bookRoutes);
-app.use("/api/carts", cartRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/carts", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
-app.listen(PORT, () => {
-    console.log("Hello World");
-    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
-});
+// Solo iniciar el servidor cuando se ejecuta localmente
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log("Hello World");
+        console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+    });
+}
+
+module.exports = app;
